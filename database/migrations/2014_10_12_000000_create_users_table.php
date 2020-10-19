9e23e81efc38ff2ae6c3aa9edfa9b3564f0a2c6c
Gopal Panadi <gopal.panadi41@gmail.com>
2020-10-20 00:58:29 +0530

    added jetstream, fortify

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -17,27 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('role');
-            $table->tinyInteger('is_active')->default(0);
+            $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->text('profile_photo_path')->nullable();
             $table->timestamps();
         });
-
-        DB::table('users')->insert([
-            [
-                'first_name' => 'Teacher', 
-                'last_name' => 'Admin',
-                'role' => 'teacher',
-                'is_active' => '1',
-                'email' => 'teacher_admin@examportal.com',
-                'password' => Hash::make('123456'),
-                'created_at' => date_create()
-            ]
-        ]);
-
     }
 
     /**
