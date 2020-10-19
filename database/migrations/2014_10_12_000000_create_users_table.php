222b722f64da0d01c5bcae3026c19a17c8076414
Gopal Panadi <gopal.panadi41@gmail.com>
2020-10-20 00:46:22 +0530

    changed user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -15,13 +17,27 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('role');
+            $table->tinyInteger('is_active')->default(0);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert([
+            [
+                'first_name' => 'Teacher', 
+                'last_name' => 'Admin',
+                'role' => 'teacher',
+                'is_active' => '1',
+                'email' => 'teacher_admin@examportal.com',
+                'password' => Hash::make('123456'),
+                'created_at' => date_create()
+            ]
+        ]);
+
     }
 
     /**
